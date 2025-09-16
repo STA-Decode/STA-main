3f12ea15ada942e1bec20f00d21253915565c440
Stanislas Tech Team <ftcscw@gmail.com>
2025-09-16 13:45:53 +0200

    Added motor and made motor.java class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/opmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/opmode.java
@@ -5,33 +5,44 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.RobotParts.driveTrain;
+import org.firstinspires.ftc.teamcode.RobotParts.Motors;
 
-
-//the name is how this Opmode will show up on the driver-hub
+//the namee is how this Opmode will show up on the driver-hub
 @TeleOp(name = "Opmode", group = "TeleOp")
 public class opmode extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     driveTrain drivetrain = new driveTrain();
-
+    Motors Motors = new Motors();
     @Override
     public void runOpMode() throws InterruptedException {
         drivetrain.init(hardwareMap);
-
+        Motors.init(hardwareMap);
 
         waitForStart();
         if (isStopRequested()) return;
+
+        double spin = 0;
 
         while (opModeIsActive()) {
             //We'll do something fun here, you will wanna read controller input, and use the methods made in the drivetrain
             double y = gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
             double rotate = -gamepad1.right_stick_x;
-            double speed = 0.7 *gamepad1.right_trigger;
+            double speed = 1 *gamepad1.right_trigger;
 
+            if (gamepad1.y)
+            {
+                spin = 1;
+            }
+
+            if (gamepad1.b)
+            {
+                spin = 0;
+            }
             drivetrain.drive(y,x,rotate,speed);
-
-
+            Motors.intakeMethod(spin);
         }
+
 
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/Motors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/Motors.java
@@ -18,52 +18,35 @@ I won't explain the basics of java, but i will explain FTC-programming. I'd reco
 
 
 package org.firstinspires.ftc.teamcode.RobotParts;
-import  com.qualcomm.robotcore.hardware.DcMotor;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class driveTrain
+public class Motors
 {
     //Here, we declare our parts and other stuff, so that the computer knows they exist, and what type they are (type like servo, motor, etc.)
-    private DcMotorEx leftFront;
-    private DcMotorEx rightFront;
-    private DcMotorEx leftBack;
-    private DcMotorEx rightBack;
+    private DcMotorEx intake;
 
     //When you start your main program you have to call init() on every class used.
     //In the init, we do stuff that has to happen exactly once in our program
     public void init(HardwareMap map){
         //Here, we are linking the motors we just declared with actual motors on our robot. In your HardwareMap, you will assign a name to different motors (making the HardwareMap doesn't happen in the code, just ask someone how to do it and be prepared for a lot of following wires)
-        leftFront = map.get(DcMotorEx.class, "left_front");
-        rightFront = map.get(DcMotorEx.class, "right_front");
-        leftBack = map.get(DcMotorEx.class, "left_back");
-        rightBack = map.get(DcMotorEx.class, "right_back");
+        intake = map.get(DcMotorEx.class, "intake");
+
 
         //These lines make sure that when the motor is unpowered, it won't move due to external forces like gravity, but it's actively trying to stay in it's current position
-        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
     }
 
     //The method rotate() is used whenever we wanna... you guessed it: rotate
-    public void rotate(double speed){
+    public void intakeMethod(double speed){
         //quick note: a negative speed will make it turn counterclockwise
 
         //Here, we are setting the power (a value between -1 and 1) to our motors
-        leftFront.setPower(speed);
-        leftBack.setPower(speed);
-        rightFront.setPower(speed);
-        rightBack.setPower(speed);
-        //IMPORTANT: if you notice the robots wheels are not turning in the direction you expected, it is probably because of the fact you used gears in your robot. This can make the turning-direction flip, so if thats the case, just put a "-" before "speed" for the motor having problems
-    }
+        intake.setPower(speed);
 
-    //The method drive() is a lot like rotate()
-    public void drive(double y, double x,double rotate, double speed){
-        leftFront.setPower(speed*(y - x + rotate));
-        leftBack.setPower(speed*(-y + x + rotate));
-        rightFront.setPower(-speed*(y + x - rotate));
-        rightBack.setPower(-speed*(-y - x - rotate));
-    }
+       }
 
 }
