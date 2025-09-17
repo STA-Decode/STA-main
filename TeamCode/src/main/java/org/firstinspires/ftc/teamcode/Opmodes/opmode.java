c2493bee2c23453ced129bf953265e3c788d3018
Stanislas Tech Team <ftcscw@gmail.com>
2025-09-17 14:06:52 +0200

    Added motor and made motor.java class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/opmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/opmode.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.RobotParts.driveTrain;
 import org.firstinspires.ftc.teamcode.RobotParts.Motors;
+import org.firstinspires.ftc.teamcode.RobotParts.Servos;
+
 
 //the namee is how this Opmode will show up on the driver-hub
 @TeleOp(name = "Opmode", group = "TeleOp")
@@ -13,15 +15,20 @@ public class opmode extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     driveTrain drivetrain = new driveTrain();
     Motors Motors = new Motors();
+    Servos Servos = new Servos();
     @Override
     public void runOpMode() throws InterruptedException {
         drivetrain.init(hardwareMap);
         Motors.init(hardwareMap);
+        Servos.init(hardwareMap);
 
         waitForStart();
         if (isStopRequested()) return;
 
         double spin = 0;
+        double chainSpeed = 0;
+        double level = 0;
+        double power = 0;
 
         while (opModeIsActive()) {
             //We'll do something fun here, you will wanna read controller input, and use the methods made in the drivetrain
@@ -39,8 +46,42 @@ public class opmode extends LinearOpMode {
             {
                 spin = 0;
             }
+
+            if (gamepad1.dpad_up)
+            {
+                chainSpeed = 1;
+            }
+
+            if (gamepad1.dpad_down)
+            {
+                chainSpeed = 0;
+            }
+
+            if (gamepad1.left_bumper)
+            {
+                level = 1;
+            }
+
+            if (gamepad1.right_bumper)
+            {
+                level = 0;
+            }
+
+            if (gamepad1.a)
+            {
+                power = 1;
+            } else {
+                power = 0;
+            }
+
+
+
+
             drivetrain.drive(y,x,rotate,speed);
             Motors.intakeMethod(spin);
+            Motors.shootingMethod(power);
+            Servos.Transport(chainSpeed);
+            Servos.Lift(level);
         }
 
 
