bad8f6a4268a4fa32eecd76dd5661897752d764e
Stanislas Tech Team <ftcscw@gmail.com>
2025-10-07 13:19:56 +0200

    Left and right shooters changed to one shooter and uncommented

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/Motors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/Motors.java
@@ -27,22 +27,19 @@ public class Motors
 {
     //Here, we declare our parts and other stuff, so that the computer knows they exist, and what type they are (type like servo, motor, etc.)
     private DcMotorEx intake;
-    private DcMotorEx leftShooter;
-    private DcMotorEx rightShooter;
+    private DcMotorEx shooter;
     private DcMotorEx chain;
     //When you start your main program you have to call init() on every class used.
     //In the init, we do stuff that has to happen exactly once in our program
     public void init(HardwareMap map){
         //Here, we are linking the motors we just declared with actual motors on our robot. In your HardwareMap, you will assign a name to different motors (making the HardwareMap doesn't happen in the code, just ask someone how to do it and be prepared for a lot of following wires)
         intake = map.get(DcMotorEx.class, "intake");
-        //leftShooter = map.get(DcMotorEx.class, "leftShooter");
-        //rightShooter = map.get(DcMotorEx.class, "rightShooter");
+        shooter = map.get(DcMotorEx.class, "shooter");
         //chain = map.get(DcMotorEx.class, "rightShooter");
 
         //These lines make sure that when the motor is unpowered, it won't move due to external forces like gravity, but it's actively trying to stay in it's current position
         intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //leftShooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //rightShooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         //chain.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
@@ -56,8 +53,8 @@ public class Motors
        }
     public void shootingMethod(double power){
 
-        //leftShooter.setPower(power);
-        //rightShooter.setPower(power);
+        shooter.setPower(power);
+
     }
     public void transport(double chainspeed){
 
